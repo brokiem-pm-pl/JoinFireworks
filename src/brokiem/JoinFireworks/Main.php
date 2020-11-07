850579fbbe1669ca71515f05a5522fc8ea53f420
brokiem <61258338+brokiem@users.noreply.github.com>
2020-11-07 16:51:05 +0800

    Add files via upload

---
+++ src/brokiem/JoinFireworks/Main.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace brokiem\JoinFireworks;
+
+use BlockHorizons\Fireworks\item\Fireworks;
+use BlockHorizons\Fireworks\entity\FireworksRocket;
+
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+	public function onEnable() {
+		ItemFactory::registerItem(new Fireworks());
+		Item::initCreativeItems(); //will load firework rockets from pocketmine's resources folder
+		if(!Entity::registerEntity(FireworksRocket::class, false, ["FireworksRocket"])) {
+			$this->getLogger()->error("Failed to register FireworksRocket entity with savename 'FireworksRocket'");
+		}
+  	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+
+	public function getFireworksType() {
+		$type_array = [
+			Fireworks::TYPE_SMALL_SPHERE,
+			Fireworks::TYPE_HUGE_SPHERE,
+			Fireworks::TYPE_STAR,
+			Fireworks::TYPE_CREEPER_HEAD,
+			Fireworks::TYPE_BURST
+		];
+
+		return $type_array[array_rand($type_array)];
+	}
+
+	public function getFireworksColor() {
+		$color_array = [
+			Fireworks::COLOR_BLACK,
+			Fireworks::COLOR_RED,
+			Fireworks::TYPE_STAR,
+			Fireworks::COLOR_DARK_GREEN,
+			Fireworks::COLOR_BROWN,
+			Fireworks::COLOR_BLUE,
+			Fireworks::COLOR_DARK_PURPLE,
+			Fireworks::COLOR_DARK_AQUA,
+			Fireworks::COLOR_GRAY,
+			Fireworks::COLOR_DARK_GRAY,
+			Fireworks::COLOR_PINK,
+			Fireworks::COLOR_GREEN,
+			Fireworks::COLOR_YELLOW,
+			Fireworks::COLOR_LIGHT_AQUA,
+			Fireworks::COLOR_DARK_PINK,
+			Fireworks::COLOR_GOLD,
+			Fireworks::COLOR_WHITE
+		];
+
+		return $color_array[array_rand($color_array)];
+	}
+
+  public function onJoin(PlayerJoinEvent $event) {
+    $fw = ItemFactory::get(Item::FIREWORKS);
+  	$fw->addExplosion($this->getFireworksType(), $this->getFireworksColor(), "", true, true);
+    $fw->setFlightDuration(mt_rand(0.5, 2));
+    $vector3 = $this->getServer()->getDefaultLevel()->getSpawnLocation()->add(0, 1, 0);
+    $entity = FireworksRocket::createEntity("FireworksRocket", $level, $nbt, $fw);
+   	if ($entity instanceof FireworksRocket) {
+      $entity->spawnToAll();
+    }
+	}
+}
